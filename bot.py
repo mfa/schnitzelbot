80fced3aaf0a3ca9ca88d2d67e1997ce873b6748
Andreas Madsack <andreas@madflex.de>
2014-09-27 15:54:38 +0200

    answer sometimes ... new replies please via pull request :)

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
+# -*- coding: utf-8 -*-
+
 # set credentials in credentials.py
 from credentials import API_KEY, API_SECRET, CLIENT_TOKEN, CLIENT_SECRET
 
 import tweepy
+import random
 import json
+from replies import replies_list
 
 
 class StdOutListener(tweepy.streaming.StreamListener):
@@ -10,11 +14,32 @@ class StdOutListener(tweepy.streaming.StreamListener):
     This is a basic listener that just prints received tweets to stdout.
 
     """
+
+    last_own_tweet = None
+
     def on_data(self, data):
         data = json.loads(data)
-        api.create_favorite(data.get('id'))
-        if data.get('user') in api.me().friends():
+        print data.get('text')
+        try:
+            # FIXME: how to test if already faved?
+            api.create_favorite(data.get('id'))
+        except:
+            pass
+        if data.get('user').get('id') in api.me().friends():
             api.create_friendship(data.get('user').get('id'))
+
+        # reply to some of the tweets
+        if random.randint(0, 10) == 4:
+            # FIXME not the best way to to this:
+            new_status = random.choice(replies_list)
+            while new_status == self.last_own_tweet:
+                new_status = random.choice(replies_list)
+            last_own_tweet = new_status
+            api.update_status(status='@%s %s' % (data.get('user').get('screen_name'),
+                                                 last_own_tweet),
+                              in_reply_to_status_id=data.get('id')
+                          )
+
         return True
 
     def on_error(self, status):
---
+++ replies.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# list of random replies
+
+replies_list = (
+    'everything is better with bacon!',
+    'enjoy your meal!',
+    u'bon appétit!',
+# add more if you like:
+)
